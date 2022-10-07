2447e3db4af13da7fe5dd2cfca68ddc7d897b2fe
Aswini <aswinikar1998@gmail.com>
2022-10-08 00:21:08 +0530

    Added POST,PUT,PATCH

--- profile_api/views.py
+++ profile_api/views.py
@@ -1,10 +1,12 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-
+from rest_framework import status
+from profile_api import serializers
 
 
 class HelloApiView(APIView):
     '''Test API views'''
+    serializer_class=serializers.HelloSerializer
 
     def get(self,request,format=None):
         """return a list if APIView feacture"""
@@ -15,3 +17,31 @@ class HelloApiView(APIView):
         "is mapped manually to urls",
         }
         return Response({"message":"hello","an_apiviews":an_apiviews})
+
+    def post(self,request):
+        """ create a hello message with our name"""
+        serializer=self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name=serializer.validated_data.get("name")
+            message=f"hello {name}"
+
+            return Response({"message":message})
+        else:
+            return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+            )
+
+
+    def put(self,request,pk=None):
+        """Handle updating a object"""
+        return Response({"method":"PUT"})
+
+    def patch(self,request,pk=None):
+        """Handle partial update of an object"""
+        return Response({"method":"PATCH"})
+
+    def delete(self,request,pk=None):
+        """delete of an object"""
+        return Response({"method":"DELETE"})
