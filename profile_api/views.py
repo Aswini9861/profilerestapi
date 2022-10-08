9b8a775a711bbcd84110c9e59f60832a2d99d13a
Aswini <aswinikar1998@gmail.com>
2022-10-09 01:49:41 +0530

    add search box in our user profile

--- profile_api/views.py
+++ profile_api/views.py
@@ -3,7 +3,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from profile_api import models, serializers
-from rest_framework import viewsets
+from rest_framework import viewsets,filters
 from rest_framework.authentication import TokenAuthentication
 from profile_api import permissions
 
@@ -102,7 +102,9 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     serializer_class=serializers.Userprofileserialiizer
     queryset=models.UserProfile.objects.all()
     authentication_classes=(TokenAuthentication,)
-    permission_classes=(permissions.Updateownprofile,)
+    permission_classes=(permissions.UpdateOwnProfile,)
+    filter_backends=(filters.SearchFilter,)
+    search_fields={'name','email',}
 
 
 
